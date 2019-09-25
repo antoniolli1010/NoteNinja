b4f91b5e5dc46f007f3d515ea3371ef88e0b8e28
Vinicius Bilhar Antoniolli <vinicius.antoniolli@pampa.compasso>
2019-09-25 15:00:15 -0300

    default changes to project

--- routes.js
+++ routes.js
@@ -1,15 +1,6 @@
 const express = require('express')
 const routes = express.Router()
-const categoryController = require('./controllers/categoryController')
 const noteController = require('./controllers/noteController')
-
-// rotas de categorias
-
-//routes.get('/categories', categoryController.index)
-//routes.get('/categories/:id', categoryController.show)
-routes.post('/categories/:id/notes', categoryController.store)
-//routes.put('/categories/:id', categoryController.update)
-//routes.delete('/categories/:id', categoryController.destroy)
 
 // rotas de anotações
 
@@ -18,9 +9,6 @@ routes.get('/notes/:id', noteController.show)
 routes.post('/notes', noteController.store)
 routes.put('/notes/:id', noteController.update)
 routes.delete('/notes/:id', noteController.destroy)
-
-
-
 
 
 module.exports = routes
